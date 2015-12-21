7d2959483f74a02aef7afec8df321b4dbc4cf388
Giulio Iotti <dullgiulio@gmail.com>
2015-12-21 16:40:03 +0200

    Licence and working source

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
+// Copyright 2015 Giulio Iotti. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,18 +21,13 @@ const (
 	backlightCurrPath = "/sys/class/backlight/intel_backlight/brightness"
 )
 
-type sysfile struct {
-	*os.File
-	name string
-}
-
-func newSysfile(f string) (*sysfile, error) {
+func sysfileReadInt(f string) (int, error) {
 	file, err := os.Open(f)
-	return &sysfile{file, f}, err
-}
-
-func (f *sysfile) readInt() (int, error) {
-	buf, err := ioutil.ReadAll(f.File)
+	if err != nil {
+		return -1, err
+	}
+	defer file.Close()
+	buf, err := ioutil.ReadAll(file)
 	if err != nil {
 		return -1, err
 	}
@@ -40,35 +40,22 @@ func sysfileWriteInt(name string, n int) error {
 	return ioutil.WriteFile(name, []byte(buf), 0644)
 }
 
-func sysfileVal(f string) (int, error) {
-	sfile, err := newSysfile(f)
-	if err != nil {
-		return -1, err
-	}
-	defer sfile.Close()
-	val, err := sfile.readInt()
-	if err != nil {
-		return -1, err
-	}
-	return val, nil
-}
-
 type poller struct {
-	max, ratio int
-	wait       time.Duration
-	dryrun     bool
+	max, min int
+	ratio    int
+	dryrun   bool
+	wait     time.Duration
 }
 
 func (p *poller) poll() {
 	maxIn := p.ratio * 100
+	blMin := p.max / 50 // TODO: This is quite a magic number
 	for {
-		blight, err := sysfileVal(backlightCurrPath)
+		blight, err := sysfileReadInt(backlightCurrPath)
 		if err != nil {
 			log.Fatal("cannot get backlight value: ", err)
 		}
-		blightPercent := blight * 100 / p.max
-		fmt.Printf("<<< %d %d%%\n", blight, blightPercent)
-		inlight, err := sysfileVal(illuminancePath)
+		inlight, err := sysfileReadInt(illuminancePath)
 		if err != nil {
 			log.Fatal("cannot get ambient light value: ", err)
 		}
@@ -76,15 +63,21 @@ func (p *poller) poll() {
 		if inlightPercent > 100 {
 			inlightPercent = 100
 		}
-		fmt.Printf(">>> %d %d%%\n", inlight, inlightPercent)
-		nblight := blightPercent * p.max / 100
+		nblight := inlightPercent*p.max/100 + p.min
+		if nblight > p.max {
+			nblight = p.max
+		}
 		diff := nblight - blight
-		if diff >= 5 {
-			fmt.Printf("+++ %d %d%%\n", nblight, nblight*100/p.max)
+		if diff < 0 {
+			diff = -diff
+		}
+		if diff >= blMin {
 			if !p.dryrun {
 				if err := sysfileWriteInt(backlightCurrPath, nblight); err != nil {
-					log.Print("cannot set backlight: ", err)
+					log.Fatal("cannot set backlight: ", err)
 				}
+			} else {
+				log.Printf("change backlight to %d%%; illuminance = %d, backlight = %d (was %d)", inlightPercent, inlight, nblight, blight)
 			}
 		}
 		time.Sleep(p.wait)
@@ -92,15 +85,22 @@ func (p *poller) poll() {
 }
 
 func main() {
-	max, err := sysfileVal(backlightMaxPath)
+	max, err := sysfileReadInt(backlightMaxPath)
 	if err != nil {
 		log.Fatal("cannot get backlight max value: ", err)
 	}
 	p := poller{
+		min:    65,
 		max:    max,
-		ratio:  30,   // lux = 1% ( + offset as detected...)
-		dryrun: true, // TODO
+		ratio:  30,    // lux = 1%
+		dryrun: false, // TODO
 		wait:   1 * time.Second,
 	}
+	flag.IntVar(&p.min, "min", p.min, "Minimum value `N` for backlight")
+	flag.IntVar(&p.max, "max", p.max, "Maximum value `N` for backlight (autodetected)")
+	flag.IntVar(&p.ratio, "ratio", p.ratio, "Ratio `R` of light change: number of lux for a 1% change in backlight")
+	flag.BoolVar(&p.dryrun, "dryrun", p.dryrun, "Do not set backlight, only print what would happen")
+	flag.DurationVar(&p.wait, "wait", p.wait, "Duration `T` between checks for changed light conditions")
+	flag.Parse()
 	p.poll()
 }
