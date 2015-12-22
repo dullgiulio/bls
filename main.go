ee1909a8029cc98d735652d5b5eca1f6b113191d
Giulio Iotti <dullgiulio@gmail.com>
2015-12-22 16:01:21 +0200

    Improve defaults and add debug flag

--- main.go
+++ main.go
@@ -42,14 +42,16 @@ func sysfileWriteInt(name string, n int) error {
 
 type poller struct {
 	max, min int
+	sens     int
 	ratio    int
 	dryrun   bool
+	debug    bool
 	wait     time.Duration
 }
 
 func (p *poller) poll() {
 	maxIn := p.ratio * 100
-	blMin := p.max / 50 // TODO: This is quite a magic number
+	blMin := p.sens * p.max / 100
 	for {
 		blight, err := sysfileReadInt(backlightCurrPath)
 		if err != nil {
@@ -71,7 +73,11 @@ func (p *poller) poll() {
 		if diff < 0 {
 			diff = -diff
 		}
-		if diff >= blMin {
+		if p.debug {
+			log.Printf("light = %d (%d%%), back-light = %d, set %d (diff %d, min %d)", inlight, inlightPercent, blight, nblight, diff, blMin)
+		}
+		// Set backlight if there is more than the minimum change thresold to adjust. Or if we are below min (level was never set.)
+		if diff >= blMin || blight < p.min {
 			if !p.dryrun {
 				if err := sysfileWriteInt(backlightCurrPath, nblight); err != nil {
 					log.Fatal("cannot set backlight: ", err)
@@ -90,16 +96,20 @@ func main() {
 		log.Fatal("cannot get backlight max value: ", err)
 	}
 	p := poller{
-		min:    65,
-		max:    max,
-		ratio:  30,    // lux = 1%
-		dryrun: false, // TODO
-		wait:   1 * time.Second,
+		min:    75,  // backlight min N
+		max:    max, // backlight max N
+		sens:   2,   // sensitivity %
+		ratio:  60,  // lux = 1%
+		dryrun: false,
+		debug:  false,
+		wait:   2 * time.Second,
 	}
 	flag.IntVar(&p.min, "min", p.min, "Minimum value `N` for backlight")
 	flag.IntVar(&p.max, "max", p.max, "Maximum value `N` for backlight (autodetected)")
+	flag.IntVar(&p.sens, "sensitivity", p.sens, "Minimum amount `S` in percent of backlight change to perform")
 	flag.IntVar(&p.ratio, "ratio", p.ratio, "Ratio `R` of light change: number of lux for a 1% change in backlight")
 	flag.BoolVar(&p.dryrun, "dryrun", p.dryrun, "Do not set backlight, only print what would happen")
+	flag.BoolVar(&p.debug, "debug", p.debug, "Print values read from sensors every wait duration")
 	flag.DurationVar(&p.wait, "wait", p.wait, "Duration `T` between checks for changed light conditions")
 	flag.Parse()
 	p.poll()
