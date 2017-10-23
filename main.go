5177dd29e2ceb3d3d7e9a4ee192834b9c0f64ac2
Giulio Iotti <dullgiulio@gmail.com>
2017-10-23 14:33:08 +0000

    loggin: use custom loggers

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ import (
 	"time"
 )
 
+var (
+	dlog *log.Logger // debug logger
+	vlog *log.Logger // verbose log
+	elog *log.Logger // error log
+)
+
 func init() {
 	runtime.GOMAXPROCS(1)
 }
@@ -58,10 +64,7 @@ type poller struct {
 }
 
 func (p *poller) poll() {
-	var (
-		warmedUp bool
-		inIndex  int
-	)
+	var inIndex int
 	inlights := make([]int, p.probes)
 	granularity := 100
 	maxIn := p.ratio * granularity
@@ -70,26 +73,23 @@ func (p *poller) poll() {
 		var err error
 		blight, err := sysfileReadInt(backlightCurrPath)
 		if err != nil {
-			log.Fatal("cannot get backlight value: ", err)
+			elog.Fatal("cannot get backlight value: ", err)
 		}
 		for {
 			inlight, err = sysfileReadInt(illuminancePath)
 			if err != nil {
-				log.Fatal("cannot get ambient light value: ", err)
+				elog.Fatal("cannot get ambient light value: ", err)
 			}
 			inlights[inIndex] = inlight
 			inIndex = (inIndex + 1) % cap(inlights)
-			if inIndex == 0 && warmedUp == false {
-				warmedUp = true
-			}
-			if warmedUp == true {
+			if inIndex == 0 {
 				break
 			}
 		}
 		inlight = 0
 		n := 0
-		// Average light in the last cap(inlights) probes
-		for i := 0; i < cap(inlights); i++ {
+		// Average light in the last inlights probes
+		for i := 0; i < len(inlights); i++ {
 			inlight += inlights[i]
 		}
 		if n > 0 {
@@ -107,17 +107,14 @@ func (p *poller) poll() {
 		if diff < 0 {
 			diff = -diff
 		}
-		if p.debug {
-			log.Printf("light = %d (%d%%), back-light = %d, set %d (diff %d, min-diff %d)", inlight, inlightPercent, blight, nblight, diff, p.sens)
-		}
+		dlog.Printf("light = %d (%d%%), back-light = %d, set %d (diff %d, min-diff %d)", inlight, inlightPercent, blight, nblight, diff, p.sens)
 		// Set backlight if there is more than the minimum change thresold to adjust. Or if we are below min (level was never set.)
 		if diff >= p.sens || blight < p.min {
+			vlog.Printf("change backlight to %d%%; illuminance = %d, backlight = %d (was %d)", inlightPercent, inlight, nblight, blight)
 			if !p.dryrun {
 				if err := p.setBlight(blight, nblight); err != nil {
-					log.Fatal("cannot set backlight: ", err)
+					elog.Fatal("cannot set backlight: ", err)
 				}
-			} else {
-				log.Printf("change backlight to %d%%; illuminance = %d, backlight = %d (was %d)", inlightPercent, inlight, nblight, blight)
 			}
 			continue // When light was changed, probe again right away
 		}
@@ -156,17 +153,13 @@ func (p *poller) setBlight(curr, set int) error {
 }
 
 func main() {
-	max, err := sysfileReadInt(backlightMaxPath)
-	if err != nil {
-		log.Fatal("cannot get backlight max value: ", err)
-	}
 	p := poller{
 		probes:   8,
-		grad:     5,   // how much to change backlight for gradual change
-		min:      40,  // backlight min N
-		max:      max, // backlight max N
-		sens:     18,  // sensitivity %
-		ratio:    20,  // lux = 1%
+		grad:     5,  // how much to change backlight for gradual change
+		min:      40, // backlight min N
+		max:      0,  // backlight max N (0 = autodetect)
+		sens:     18, // sensitivity %
+		ratio:    20, // lux = 1%
 		dryrun:   false,
 		debug:    false,
 		wait:     4 * time.Second,
@@ -175,7 +168,7 @@ func main() {
 	flag.IntVar(&p.grad, "animation-steps", p.grad, "Number `N` of backlight to add or remove to smoothly change backlight")
 	flag.IntVar(&p.probes, "probes", p.probes, "Number `N` of illuminance probes to average")
 	flag.IntVar(&p.min, "min", p.min, "Minimum value `N` for backlight")
-	flag.IntVar(&p.max, "max", p.max, "Maximum value `N` for backlight (autodetected)")
+	flag.IntVar(&p.max, "max", p.max, "Maximum value `N` for backlight (0 = autodetected)")
 	flag.IntVar(&p.sens, "sensitivity", p.sens, "Minimum amount `S` in percent of backlight change to perform")
 	flag.IntVar(&p.ratio, "ratio", p.ratio, "Ratio `R` of light change: number of lux for a 1% change in backlight")
 	flag.BoolVar(&p.dryrun, "dryrun", p.dryrun, "Do not set backlight, only print what would happen")
@@ -183,5 +176,25 @@ func main() {
 	flag.DurationVar(&p.wait, "wait", p.wait, "Duration `T` between checks for changed light conditions")
 	flag.DurationVar(&p.gradWait, "animation", p.gradWait, "Duration `T` for smooth animation on light change")
 	flag.Parse()
+	dlogOut := ioutil.Discard
+	vlogOut := ioutil.Discard
+	elogOut := os.Stderr
+	if p.dryrun {
+		vlogOut = os.Stdout
+	}
+	if p.debug {
+		dlogOut = os.Stdout
+		vlogOut = os.Stdout
+	}
+	elog = log.New(elogOut, "bls: error: ", log.LstdFlags)
+	dlog = log.New(dlogOut, "bls: debug: ", log.LstdFlags)
+	vlog = log.New(vlogOut, "bls: info: ", log.LstdFlags)
+	if p.max == 0 {
+		var err error
+		p.max, err = sysfileReadInt(backlightMaxPath)
+		if err != nil {
+			elog.Fatal("cannot get backlight max value: ", err)
+		}
+	}
 	p.poll()
 }
